2cf5b1ff7441f6de734e7e48260608924dcc748a
Ashwin Ramesh <ashwin@detecttechnologies.com>
2021-12-23 16:14:35 +0530

    Initial import
    Copied at 3dd3a5455a267e321b3be488ced03bf2551133de from internal repo

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dt-face-blur-api",
+    version="0.1.0",
+    description="Face Blur API Client",
+    url="https://detecttechnologies.com/",
+    author="Detect Technologies Pvt. Ltd.",
+    author_email="support@detecttechnologies.com",
+    license="BSD 3-clause",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    install_requires=["pathlib>=1.0.1", "opencv-python>=4.2.0.32", "requests>=2.23.0", "logzero>=1.7.0"],
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.5",
+    ],
+)
---
+++ src/dt_face_blur_api/client.py
@@ -0,0 +1,111 @@
+import base64
+import json
+import time
+
+import cv2
+import numpy as np
+import requests
+from logzero import logger
+from requests import RequestException
+
+
+class FaceBlurAPI:
+    def __init__(self, api_url, username, password):
+        """FaceBlurAPI constructor
+
+        Args:
+            api_url (str): API URL
+            username (str): Login username
+            password (str): Login Password
+        """
+        self.api_url = api_url
+        self.username = username
+        self.password = password
+        self.auth_token = ""
+        self.login()
+
+    def login(self):
+        """Login Method"""
+        payload = json.dumps({"username": self.username, "password": self.password})
+        headers = {"Content-Type": "application/json"}
+        try:
+            response = requests.post(f"{self.api_url}/api/token/", headers=headers, data=payload)
+            self.auth_token = json.loads(response.content).get("access")
+            logger.info("Login Successful")
+        except Exception as e:
+            logger.error(e)
+
+    def return_sub6m_json(self, img):
+        """Progressively increases compression of image to generate JSON of size <6MB
+
+        Args:
+            img (np.array): Input image
+
+        Returns:
+            json_str (str): Returns sub 6Mb json payload
+        """
+        jpeg_quality = 100
+        target_size = 6000
+        while jpeg_quality > 50:
+            encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), jpeg_quality]
+            img_bytes = cv2.imencode(".jpg", img, encode_param)[1].tobytes()
+            img_str = base64.b64encode(img_bytes).decode("utf-8")
+            data = {"img": img_str, "jpeg_quality": jpeg_quality}
+            data_json = json.dumps(data)
+            logger.info(f"data_json size in KB: {len(data_json)/1024}")
+
+            if len(data_json) / 1024 < target_size:
+                return data_json
+            jpeg_quality -= 5
+        return None
+
+    def blur_np(self, img):
+        """Blurs image given as np.array/cv2 image
+
+        Args:
+            img (np.array): Input image
+
+        Returns:
+            img (np.array): Face blurred image
+        """
+        data_json = self.return_sub6m_json(img)
+
+        if not data_json:
+            logger.error("Image too large to compress")
+            return None
+
+        headers = {"Authorization": f"Bearer {self.auth_token}", "Content-Type": "application/json"}
+        MAX_RETRY = 3
+        retries = 0
+        while retries <= MAX_RETRY:
+            try:
+                response = requests.post(url=f"{self.api_url}/blur/", headers=headers, data=data_json)
+                response.raise_for_status()
+                break
+            except RequestException as e:
+                retries += 1
+                if retries <= MAX_RETRY:
+                    logger.error(f"blur_cv2 failed try #{retries}")
+                    time.sleep(2)
+                    if e.response.status_code == 403:
+                        # Auth token expired, login again
+                        self.login()
+                else:
+                    raise RequestException(e)
+        d = json.loads(response.content)
+        im_bytes = base64.b64decode(d["img"])
+        im_arr = np.frombuffer(im_bytes, dtype=np.uint8)  # im_arr is one-dim Numpy array
+        img = cv2.imdecode(im_arr, flags=cv2.IMREAD_COLOR)
+        return img
+
+    def blur_path(self, path):
+        """Blurs image given as a path
+
+        Args:
+            path (str): Input image path
+
+        Returns:
+            img (np.array): Face blurred image
+        """
+        img = cv2.imread(str(path))
+        return self.blur_cv2(img)
